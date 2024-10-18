ff628e7353978ebb27c397a203413029f4358139
ARCHIT K CHERIAN <archithrd@gmail.com>
2024-10-18 10:02:57 -0400

    [development_2] adding the components

---
+++ src/app/website/category-products/category-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './category-products.component.html',
+  styleUrl: './category-products.component.css'
+})
+export class CategoryProductsComponent {
+
+}
---
+++ src/app/website/web-products/web-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './web-products.component.html',
+  styleUrl: './web-products.component.css'
+})
+export class WebProductsComponent {
+
+}
