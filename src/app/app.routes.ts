e2527ff42845e319477e086a3869ed95abecac0f
ARCHIT K CHERIAN <archithrd@gmail.com>
2024-10-18 10:57:37 -0400

    [developement_2] - boostrapping and style with layout login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './admin/login/login.component';
+import { ProductsComponent } from './admin/products/products.component';
+import { LayoutComponent } from './admin/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: '', component: LayoutComponent, children: [{path: 'products', component: ProductsComponent}]}, 
+];
